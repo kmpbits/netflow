0d337b3d473ceb301d8e09c9eddeb99f21594b0d
KMP Bits <kmpbits@gmail.com>
2025-10-30 11:57:52 +0000

    Refactor: Use `header` instead of `addHeader` for custom headers
    This commit changes the implementation of the `CustomHeaderInterceptor` to use the `header` function instead of `addHeader`.
    
    Using `header` ensures that if a header with the same name already exists, it is replaced with the new value. In contrast, `addHeader` would append another header with the same name, which could lead to unexpected behavior. This change makes the header injection more robust and predictable.

--- netflow-core/build.gradle.kts
+++ netflow-core/build.gradle.kts
@@ -49,7 +49,7 @@ kotlin {
 
 val artifactCoreId = "netflow-core"
 val groupGitHubId = "io.github.kmpbits"
-val libraryVersion = "0.0.16-alpha"
+val libraryVersion = "0.0.17-alpha"
 
 group = groupGitHubId
 version = libraryVersion
