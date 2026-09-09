8c41d17a19ee218754c686b15e59ec08cfff3bc8
KMP Bits <kmpbits@gmail.com>
2026-09-09 21:06:11 +0100

    feat: wire multipart bodies into the iOS request build path

--- netflow-core/src/iosMain/kotlin/com/kmpbits/netflow_core/builders/RequestBuilder.ios.kt
+++ netflow-core/src/iosMain/kotlin/com/kmpbits/netflow_core/builders/RequestBuilder.ios.kt
@@ -31,9 +31,18 @@ internal actual fun RequestBuilder.build(): InternalHttpRequestBuilder {
             setValue(it.second, forHTTPHeaderField = it.first.header)
         }
 
-        val bodyString = rawBody ?: body?.toJsonString()
-        if (bodyString != null && allowsBody) {
-            HTTPBody = bodyString.toNSData()
+        if (parts.isNotEmpty()) {
+            val boundary = multipartBoundary()
+            setValue(
+                "multipart/form-data; boundary=$boundary",
+                forHTTPHeaderField = "Content-Type",
+            )
+            HTTPBody = buildMultipartBody(parts, boundary)
+        } else {
+            val bodyString = rawBody ?: body?.toJsonString()
+            if (bodyString != null && allowsBody) {
+                HTTPBody = bodyString.toNSData()
+            }
         }
     }
 
